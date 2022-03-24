ca0c3a44258bff0bd8b49898eca4d9ad93573c3a
carmellemillar <carmelle@stanford.edu>
2022-03-24 13:29:47 -0400

    add another p tag

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>This is change #1</p>
+        <p>look another p tag</p>
         <a
           className="App-link"
           href="https://reactjs.org"
